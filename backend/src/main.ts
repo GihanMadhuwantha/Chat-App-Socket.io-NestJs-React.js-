fd35a4bbf3ca1686bffdc3ee2a9e1e144949af80
gihanmadhuwantha98 <gihan.madhuwantha@layoutindex.lk>
2024-11-04 17:24:51 +0530

    cors fix -login,register

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "http://localhost:3000", // Allow only your frontend origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Allow cookies if needed
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
